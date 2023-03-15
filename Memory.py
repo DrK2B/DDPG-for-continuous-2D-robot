e76e0c437763fb9d6bc618710b69a00be985f071
Kaosi <begbuonu@gmail.com>
2023-03-15 21:15:18 +0100

    Initial commit

---
+++ Memory.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+class ReplayMemory:
+    def __int__(self, mem_size, state_shape, num_actions):  # num_actions: number of components to the action vector
+        self.mem_size = mem_size
+        self.mem_cntr = 0
+        self.state_memory = np.zeros((self.mem_size, *state_shape))
+        self.action_memory = np.zeros((self.mem_size, num_actions))
+        self.reward_memory = np.zeros(self.mem_size)
+        self.new_state_memory = np.zeros((self.mem_size, *state_shape))
+        self.done_memory = np.zeros(self.mem_size, dtype=np.bool)
+
+    def store_transition(self, state, action, reward, new_state, done):
+        index = self.mem_cntr % self.mem_size
+
+        self.state_memory[index] = state
+        self.action_memory[index] = action
+        self.reward_memory[index] = reward
+        self.new_state_memory[index] = new_state
+        self.done_memory[index] = done
+
+        self.mem_cntr += 1
+
+    def sample(self, batch_size):
+        max_mem = min(self.mem_cntr, self.mem_size)
+
+        batch = np.random.choice(max_mem, batch_size, replace=False)
+
+        states = self.state_memory[batch]
+        actions = self.action_memory[batch]
+        rewards = self.reward_memory[batch]
+        new_states = self.new_state_memory[batch]
+        dones = self.done_memory[batch]
+
+        return states, actions, rewards, new_states, dones
+
+
