e15e6d33381893a482869d5e7b351e8209fc8578
Kaosi <begbuonu@gmail.com>
2023-03-21 19:08:36 +0100

    minor change

--- _main.py
+++ _main.py
@@ -3,24 +3,24 @@ import numpy as np
 import tensorflow as tf
 from Agent import ddpgAgent
 from Noise import OUNoise
-from utils import plot_learning_curve, save_learningCurveData_to_csv, create_unique_filename
+from utils import plot_learningCurve, save_learningCurveData_to_csv, create_unique_filename
 
 
 def DDPG():
     # Hyperparameters
     HPARAMS = {
-        "Episodes": 1000,
+        "Episodes": 10,
         "Time steps": 500,
         "Explorations": 0,  # number of episodes with (random) exploration only
-        "Critic learning rate": 0.02,
-        "Actor learning rate": 0.01,
+        "Critic learning rate": 0.002,
+        "Actor learning rate": 0.001,
         "Discount factor": 0.99,
         "Memory size": 100000,
         "Polyak averaging": 0.005,
-        "Critic layer sizes": (10, 10),  # critic networks are designed to have 2 hidden layers
-        "Actor layer sizes": (10, 10),  # actor networks are designed to have 2 hidden layers
+        "Critic layer sizes": (8, 8),  # critic networks are designed to have 2 hidden layers
+        "Actor layer sizes": (8, 8),  # actor networks are designed to have 2 hidden layers
         "Batch size": 64,
-        "Noise std. dev.": 0.25  # std dev of zero-mean gaussian distributed noise
+        "Noise std. dev.": 0.2  # std dev of zero-mean gaussian distributed noise
     }
 
     # settings
@@ -53,7 +53,6 @@ def DDPG():
         agent.learn()
         agent.load_models()
 
-    # ToDo: add termination criterion so that training stops if there is no improvement over several episodes (optional)
     for episode in range(1, HPARAMS["Episodes"] + 1):
         state = env.reset()[0]
         noise.reset()
@@ -87,6 +86,7 @@ def DDPG():
                 agent.save_models()
                 print("models' parameters saved at episode: ", episode)
 
+        plot_learningCurve(score_history, ROLLING_WINDOW_SIZE_AVG_SCORE, **HPARAMS)
         print("Completed in {} steps.... episode: {}/{}, episode reward: {},"
               " average episode reward: {}".format(time_len, episode, HPARAMS["Episodes"], score, avg_score))
 
@@ -94,9 +94,10 @@ def DDPG():
     env.close()
 
     if not EVALUATE:
-        ENV_NAME = create_unique_filename(ENV_NAME)
-        save_learningCurveData_to_csv(score_history, ENV_NAME)
-        plot_learning_curve(score_history, ENV_NAME, ROLLING_WINDOW_SIZE_AVG_SCORE, **HPARAMS)
+        # save data and plot learning curve
+        filename = create_unique_filename(ENV_NAME)
+        save_learningCurveData_to_csv(score_history, filename)
+        plot_learningCurve(score_history, ROLLING_WINDOW_SIZE_AVG_SCORE, filename=filename, **HPARAMS)
 
     print('--- Finished DDPG ---')
 
--- utils.py
+++ utils.py
@@ -5,13 +5,13 @@ import csv
 from datetime import datetime
 
 
-def plot_learning_curve(scores, filename, rolling_window_size=100, **hyperparameters):
+def plot_learningCurve(scores, rolling_window_size=100, filename=None, **hyperparameters):
     running_avg = np.zeros(len(scores))
     for i in range(len(running_avg)):
         running_avg[i] = np.mean(scores[max(0, i - rolling_window_size):(i + 1)])
 
     fig, ax = plt.subplots()
-    ax.plot(range(1, len(running_avg)+1), running_avg)
+    ax.plot(range(1, len(running_avg) + 1), running_avg)
     ax.set_title('Running average of previous %d episode rewards' % rolling_window_size)
     plt.xlabel('episode')
     plt.ylabel('average episode reward')
@@ -22,12 +22,16 @@ def plot_learning_curve(scores, filename, rolling_window_size=100, **hyperparame
     properties = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
     ax.text(0.65, 0.05, infos, transform=ax.transAxes, fontsize=8, verticalalignment='bottom', bbox=properties)
 
-    filepath = os.path.join('tmp/figures', filename + '_plot.png')
     # Set environment variable to avoid multiple loading of a shared library
     os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-    plt.savefig(filepath)
+
+    if filename is not None:
+        # save the plot of the learning curve
+        filepath = os.path.join('tmp/figures', filename + '_plot.png')
+        plt.savefig(filepath)
+        print("Saved learning curve plot")
+
     plt.show()
-    print("Saved learning curve plot")
 
 
 def save_learningCurveData_to_csv(scores, filename):
@@ -35,8 +39,8 @@ def save_learningCurveData_to_csv(scores, filename):
     with open(filepath, 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(['x', 'y'])
-        for i in range(1, len(scores)+1):
-            writer.writerow([i, scores[i-1]])
+        for i in range(1, len(scores) + 1):
+            writer.writerow([i, scores[i - 1]])
     print("Saved learning curve data")
 
 
