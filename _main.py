5155b2fc70e262a0b051a70e48cecc4070649a04
Kaosi <begbuonu@gmail.com>
2023-04-01 12:08:08 +0200

    minor changes and update

--- _main.py
+++ _main.py
@@ -12,16 +12,16 @@ def DDPG():
         "Episodes": 1000,
         "Time steps": 500,
         "Explorations": 0,  # number of episodes with (random) exploration only (and no exploitation)
-        "Critic learning rate": 0.002,
-        "Actor learning rate": 0.001,
+        "Critic learning rate": 0.001,
+        "Actor learning rate": 0.0001,
         "Discount factor": 0.99,
-        "Memory size": 100000,
+        "Memory size": 1000000,
         "Polyak averaging": 0.001,
-        "Critic layer sizes": (16, 16),  # number of hidden layers is variable and corresponds to tuple length
-        "Actor layer sizes": (16, 16),  # number of hidden layers is variable and corresponds to tuple length
-        "Batch size": 32,
-        "Noise type": "Gaussian",
-        "Noise std. dev.": 0.25  # std dev of zero-mean gaussian distributed noise
+        "Critic layer sizes": (8, 8),  # number of hidden layers is variable and corresponds to tuple length
+        "Actor layer sizes": (8, 8),  # number of hidden layers is variable and corresponds to tuple length
+        "Batch size": 64,
+        "Noise type": "OU",
+        "Noise std. dev.": 0.3  # std dev of zero-mean gaussian distributed noise
     }
 
     # settings
